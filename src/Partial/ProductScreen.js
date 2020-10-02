dda9345d63e41769be05e052c22f7b9ef1696269
Kawsar Ahmed <ksabunny@gmail.com>
2020-10-02 17:11:23 +0930

    Homescreen and product screen update

---
+++ src/Partial/ProductScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import data from '../data'
+import { Link } from 'react-router-dom';
+
+function ProductScreen(props){
+    console.log(props.match.params.id)
+    const product = data.products.find(x=> x._id=== props.match.params.id);
+
+    return <div>
+        <div className="back-to-result">
+            <Link to="/"><b>Back to menu</b></Link>
+        </div>
+        <div className="details">
+            <div className="details-image">
+                <img src={product.image} alt="product"></img>
+            </div>
+            <div className="details-info">
+                <ul>
+                    <li>
+                        <h4> {product.name}</h4>
+                    </li>
+                    <li>
+                        Price: <b>AUD {product.price}</b>
+                    </li>
+                </ul>
+            </div>
+            <div className="details-action">
+                <ul>
+                    <li>
+                        Price: AUD {product.price}
+                    </li>
+                    <li>
+                        Status: {product.status}
+                    </li>
+                    <li>
+                        Qty: <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </li>
+                    <li>
+                        <button className="crt_button">Add to Cart</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+}
+
+export default ProductScreen;
